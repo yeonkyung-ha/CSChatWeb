e0b35533f75a3af3e8699b16d722e8763a8ff944
하연경 <hayeongyeong@hayeongyeong-ui-MacBookAir.local>
2023-12-02 19:46:57 +0900

    Create Home/Login/Profile/Registration/GroupChat pages; Complete routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Registration from './components/Registration';
+import GroupChat from './components/GroupChat';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Registration" element={<Registration />} />
+          <Route path="/GroupChat" element={<GroupChat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/GroupChat.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function GroupChat() {
+  return (
+    <div>
+      <h1>Group Chat</h1>
+    </div>
+  );
+}
+export default GroupChat;

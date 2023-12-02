5e59ea7ab9fc95390c0e9abd42a5aa4541a6b36f
하연경 <hayeongyeong@hayeongyeong-ui-MacBookAir.local>
2023-12-02 19:56:30 +0900

    Create Navigation Bar

--- src/components/GroupChat.jsx
+++ src/components/GroupChat.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 function GroupChat() {
   return (
     <div>
       <h1>Group Chat</h1>
+      <Navbar key="md" expand="md">
+        <Container fluid>
+          <Navbar.Toggle id="menuButton"></Navbar.Toggle>
+          <Navbar.Offcanvas placement="end" id="navigation">
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1">
+                <Link to="/Home" className="nav-link" id="menu_item">
+                  Home
+                </Link>
+                <Link to="/Login" className="nav-link" id="menu_item">
+                  Login
+                </Link>
+                <Link to="/Profile" className="nav-link" id="menu_item">
+                  Profile
+                </Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </div>
   );
 }
